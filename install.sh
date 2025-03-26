10741ac036caeea599ae0a99fb9d912180fea1f0
Vasili <huzeacostas1345@gmail.com>
2025-03-26 14:59:26 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,6 +14,26 @@ sudo pacman -Syu --noconfirm
 
 clear
 
+read -p "Do you want to automatically install some useful tools (~40GB)? (y/n) " -n 1 -r
+echo
+case "$REPLY" in
+    [Yy]) 
+        echo "Installing..."
+        sudo pacman -S blackarch-webapp blackarch-proxy blackarch-disassembler blackarch-networking blackarch-forensic blackarch-crypto blackarch-wireless blackarch-binary blackarch-reversing blackarch-malware blackarch-wordlist blackarch-decompiler blackarch-debugger blackarch-stego
+        # yes action
+        ;;
+    [Nn])
+        echo "Okay, not installing"
+        printf "\n"
+        # no action
+        ;;
+    *)
+        echo "Invalid input, not installing"
+        printf "\n"
+        # handle invalid input
+        ;;
+esac
+
 echo "BlackArch is now installed!"
 printf "\n"
 echo "You can see available categories with:"
